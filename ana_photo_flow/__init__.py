f0037473f229557f4ead4352a7b20f10edf4c531
Yann Voté <ygversil@lilo.org>
2019-05-12 15:46:06 +0200

    Add locations where to search for config file
    Order is important as later locations will override sooner locations.
    
    Locations are in this order :
    
    * /etc/xdg/ana_photo_flow.conf
    
    * $HOME/.config/ana_photo_flow.conf
    
    * path indicated by ANA_PHOTO_FLOW_CONF environment variable.

--- ana_photo_flow/__init__.py
+++ ana_photo_flow/__init__.py
@@ -1,12 +1,24 @@
 """ana_photo_flow main package."""
 
 import logging
+import os
+import os.path
+
+import xdg
 
 
 __version__ = '0.1.0-dev2'
 __author__ = 'Yann Voté <ygversil@lilo.org>'
 __all__ = []
 
+
+# Where to search for config files
+# Order is important: each file overrides settings from previous files in this list
+_CONFIG_LOCATIONS = [
+    *(os.path.join(folder, 'ana_photo_flow.conf') for folder in xdg.XDG_CONFIG_DIRS),
+    os.path.join(xdg.XDG_CONFIG_HOME, 'ana_photo_flow.conf'),
+    os.environ.get('ANA_PHOTO_FLOW_CONF')
+]
 
 _DEFAULT_CONFIG = {
     'celery': {
